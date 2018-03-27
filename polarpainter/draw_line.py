78982e8049cb76058820869dfa97a48fe5b2fcfc
Peter F. Klemperer <peter@peterklemperer.com>
2018-03-27 17:09:59 -0400

    draws a line according to calculation

--- polarpainter/draw_line.py
+++ polarpainter/draw_line.py
@@ -1,4 +1,7 @@
 #!/anaconda3/bin/python3
+#
+# for details see
+# https://docs.google.com/spreadsheets/d/1bt1JF5_E65BAOLYqf0U_qrlKmlzXkmeIPwlu2J2Ue1o/edit?usp=sharing
 
 import subprocess
 from time import sleep
@@ -9,16 +12,35 @@ def set_motor_speeds( left, right, seconds ):
         subprocess.run(args=['ticcmd', '-d', '00203494', '-y', str(-1*right)])
         sleep(0.1)
 
+def set_motor_positions( left_cm, right_cm ):
+    """ set the motor position in ticks """
+    TICKS_PER_CM = 1000
+    resume_motors()
+    subprocess.run(args=['ticcmd', '-d', '00203410', '-p', str(left_cm * TICKS_PER_CM)])
+    subprocess.run(args=['ticcmd', '-d', '00203494', '-p', str(-1 * right_cm * TICKS_PER_CM)])
+    sleep(0.1)
+
 def resume_motors():
     subprocess.run(args=['ticcmd', '-d', '00203494', '--halt-and-hold'])
     subprocess.run(args=['ticcmd', '-d', '00203494', '--resume'])
     subprocess.run(args=['ticcmd', '-d', '00203410', '--halt-and-hold'])
     subprocess.run(args=['ticcmd', '-d', '00203410', '--resume'])
 
+def reset_positions():
+    """ stop the motors and make encoder position zero) """
+    subprocess.run(args=['ticcmd', '-d', '00203410', '--halt-and-set-position', '0'])
+    subprocess.run(args=['ticcmd', '-d', '00203494', '--halt-and-set-position', '0'])
+
+# should draw a vertical line downwards,
+# if start position is centered between
+# the motors
+
+reset_positions()
 resume_motors()
-set_motor_speeds( -1000000, -1000000, 5.0 ) # up
-set_motor_speeds( 1000000, -1000000, 5.0 ) # right
-set_motor_speeds( 1000000, 1000000, 5.0 ) # down
-set_motor_speeds( -1000000, 1000000, 5.0 ) # left
-set_motor_speeds( 0, 0, 0.5 )
+set_motor_positions(1.414213562, 1.414213562)
+set_motor_positions(2.059126028, 2.059126028)
+set_motor_positions(2.785677655, 2.785677655)
+set_motor_positions(3.544009029, 3.544009029)
+set_motor_positions(4.317406629, 4.317406629)
+set_motor_positions(5.099019514, 5.099019514)
 
