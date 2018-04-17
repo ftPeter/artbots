0b78e84455d93052284cfa1bf977c7a5663c758f
Katie Kelso <kelso22k@mtholyoke.edu>
2018-04-17 17:07:56 -0400

    trying to draw a diamond. it draws a sock

--- polarpainter/cart_to_bot.py
+++ polarpainter/cart_to_bot.py
@@ -3,8 +3,9 @@ import math
 def cartbot (x,y):
     #input is cartesian x y starting at the top left of the board
     #return value is length of left and right strings
-    distance_between_robots =32 #values in inches
+    distance_between_robots =37 #values in inches
     left_length = math.sqrt(x*x + y*y)
     right_length = math.sqrt((2-x)*(2-x) + y*y)
+    print("{},{} <= cartbot({},{})".format(left_length,right_length,x,y))
     return left_length, right_length
 
--- polarpainter/draw_line.py
+++ polarpainter/draw_line.py
@@ -5,39 +5,60 @@ import subprocess
 from time import sleep
 from cart_to_bot import cartbot
 
+right_motor_address = '00203410'
+left_motor_address = '00203494'
+
 def set_motor_positions( left_in, right_in ):
     """ set the motor position in ticks """
-    TICKS_PER_CM = 1000
-    INCHES_PER_CM = 2.54
+    TICKS_PER_IN = 125
+
+    left_tics = 1 * left_in * TICKS_PER_IN
+    right_tics =  right_in * TICKS_PER_IN
+    print (left_tics, right_tics)
+
+
     resume_motors()
-    subprocess.run(args=['ticcmd', '-d', '00203410', '-p', str(int(-1 * left_in * TICKS_PER_CM * INCHES_PER_CM))])
-    subprocess.run(args=['ticcmd', '-d', '00203494', '-p', str(int(1 * right_in * TICKS_PER_CM * INCHES_PER_CM))])
-    sleep(1)
+    subprocess.run(args=['ticcmd', '-d', right_motor_address, '-p', str(int(right_tics))])
+    subprocess.run(args=['ticcmd', '-d', left_motor_address, '-p', str(int(left_tics))])
+    sleep(4)
 
 def resume_motors():
-    subprocess.run(args=['ticcmd', '-d', '00203494', '--halt-and-hold'])
-    subprocess.run(args=['ticcmd', '-d', '00203494', '--resume'])
-    subprocess.run(args=['ticcmd', '-d', '00203410', '--halt-and-hold'])
-    subprocess.run(args=['ticcmd', '-d', '00203410', '--resume'])
+    #subprocess.run(args=['ticcmd', '-d', left_motor_address, '--halt-and-hold'])
+    subprocess.run(args=['ticcmd', '-d', left_motor_address, '--resume'])
+    #subprocess.run(args=['ticcmd', '-d', right_motor_address, '--halt-and-hold'])
+    subprocess.run(args=['ticcmd', '-d', right_motor_address, '--resume'])
 
-def reset_positions(left,right):
+def reset_positions(left_in,right_in):
     """ stop the motors and make encoder position zero) """
-    left_tics = left*1000*2.54
-    right_tics = right*1000*2.54
-    subprocess.run(args=['ticcmd', '-d', '00203410', '--halt-and-set-position', str(int(left_tics))])
-    subprocess.run(args=['ticcmd', '-d', '00203494', '--halt-and-set-position', str(int(right_tics))])
+    left_tics = left_in*125
+    right_tics = right_in*125
+    print (left_tics, right_tics)
+
+    subprocess.run(args=['ticcmd', '-d', right_motor_address, '--halt-and-set-position', str(int(right_tics))])
+    subprocess.run(args=['ticcmd', '-d', left_motor_address, '--halt-and-set-position', str(int(left_tics))])
+
+
+def print_status():
+    subprocess.run(args=['ticcmd', '-d', left_motor_address, '--status', '--full'])
+    subprocess.run(args=['ticcmd', '-d', right_motor_address, '--status', '--full'])
+
 
 
 # should draw a vertical line downwards,
 # if start position is centered between
 # the motors
-
-
 resume_motors()
-left,right = cartbot (16,16)
-reset_positions(16,16)
-print (left, right)
+reset_positions(22, 22)
+set_motor_positions(30, 22 )
+set_motor_positions(30, 30 )
+set_motor_positions(22, 30)
+set_motor_positions(22, 22 )
+#print_status()
+
+#left,right = cartbot (16,16)
+
+#print (left, right)
 
 
-target_left, target_right = cartbot (9,24)
-set_motor_positions(16, 16 )
+
+
